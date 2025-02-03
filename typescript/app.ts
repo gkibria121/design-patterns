064e95452596b160eac3aedc706b8196d2bf8220
gkibria <gkibria121@gmail.com>
2025-02-03 08:19:56 +0600

    add vite

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,1 +1,41 @@
-console.log("hello world");
+interface ProductInterface {
+  id: string;
+  price: number;
+  description: string;
+  display(): void;
+}
+
+class Product implements ProductInterface {
+  constructor(public id: string, public price: number, public description: string) {}
+  public display(): void {
+    console.log(`ID : ${this.id} \nPrice: ${this.price} \nDescription : ${this.description}`);
+  }
+}
+
+class Book extends Product {
+  constructor(
+    public author: string,
+    public title: string,
+    public id: string,
+    public price: number,
+    public description: string
+  ) {
+    super(id, price, description);
+  }
+}
+
+class Electronic extends Product {
+  constructor(
+    public brand: string,
+    public model: string,
+    public id: string,
+    public price: number,
+    public description: string
+  ) {
+    super(id, price, description);
+  }
+}
+
+let book = new Book("kibria", "The song of ice and fire", "1", 100, "This is a nice book");
+
+book.display();
